8bc68691ba48f592d34610452b0e8b62fd3664b5
homexhome <homexhome@proton.me>
2023-11-24 17:24:46 +0300

    Swagger wip 2

--- BlogFinalTask.Data/Repository/ArticleRepository.cs
+++ BlogFinalTask.Data/Repository/ArticleRepository.cs
@@ -3,6 +3,7 @@ using BlogFinalTask.Data.DTOS;
 using BlogFinalTask.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Security.Claims;
 
 namespace BlogFinalTask.Data.Repository
@@ -20,7 +21,16 @@ namespace BlogFinalTask.Data.Repository
             List<ArticleDTO> result = mapper.Map<List<ArticleDTO>>(entities);
             return result;
         }
+
+        /// <summary>
+        /// Возвращает статью по её id.
+        /// </summary>
+        /// <param name="id">ID по которому возвращается статья. </param>
+        /// <returns>Статья с определённым ID.</returns>
+        /// <response code="200">Возвращает запрошенную статью.</response>
+        /// <response code="500">Если не существует такой статьи - выкинет ошибку</response>
         [HttpGet("getArticleById")]
+        [SwaggerOperation(Summary = "Возвращает статью по её id.")]
         public async Task<ArticleDTO> GetById(string id) {
             Article? entitiy = await context.Set<Article>().FirstOrDefaultAsync(a => a.Id == id);
             if (entitiy is not null) {
